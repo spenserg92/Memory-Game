8ec5009605cfee743c0b1fb3e60d762f5394bcda
spenserg92 <spenserg92@gmail.com>
2023-11-29 16:04:31 -0500

    styling updates for now

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+// Pseudocode
+
+// Board is intialized
+// There are 18 tiles on the board
+// There are 9 matches
+// User clicks on a tile to start the game
+// User has 3 tries to guess all the matches
+// When tile is clicked it flips to show image
+// if match is made both tiles stay flipped
+// if match is not made tiles flip back to original position
+// if user runs out of guesses show "Sorry, you lose." message.
+// if user guessses all matches correctly show "Congrats, you win!" message
+// User can start a new game by clicking the "new game" button
+
+
 /*----- constants -----*/
 
 
